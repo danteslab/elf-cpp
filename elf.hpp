4aefb6897a4a6ecee0cf9fe91ff320e23820a9e2
Dante Calderón <dantehemerson@gmail.com>
2018-08-27 14:39:12 -0500

    Convertions

--- elf.hpp
+++ elf.hpp
@@ -10,3 +10,41 @@ std::string toLowerCase(std::string s) {
 	return s;
 }
 
+/**
+*	Convert an integer in string to integer.
+*
+*	@param n The number to convert.
+*	@return An integer of n.
+*   COMPLEXITY = O(1)
+**/
+int parseInt(const std::string& numString, int base = 10) {
+	std::istringstream istr(numString);
+	int value;
+	istr >> value;
+	return value;
+}
+
+/**
+*	Convert an integer to string.
+*
+*	@param number The number to convert.
+*	@return The number n in string.
+*   COMPLEXITY = O(n), n = length of number
+**/
+std::string toString(int number) {
+	if (number == 0) return "0";
+	size_t pos = 0;
+	std::string value;
+	if(number < 0) { // Negative
+		value = "-";
+		pos = 1;
+		number *= -1;
+	}
+	char mod;
+	while(number > 0) {
+		mod = number % 10;		
+		value.insert(pos, 1, mod + 48);
+		number /= 10;
+	}
+	return value;
+}
--- test.cpp
+++ test.cpp
@@ -10,3 +10,37 @@ using namespace std;
 TEST_CASE("Hello World! to uppercase (pass)", "[single-file]") {
 	REQUIRE(toUpperCase("Hello World!") == "HELLO WORLD!");
 }
+
+TEST_CASE("Empty string to uppercase", "[single-file]") {
+	REQUIRE(toUpperCase("") == "");
+}
+
+
+TEST_CASE("WELcoME To BRUsSELS to lowercase", "[single-file]") {
+	REQUIRE(toLowerCase("WELcoME To BRUsSELS") == "welcome to brussels");
+}
+
+TEST_CASE("Empty string to lowercase", "[single-file]") {
+	REQUIRE(toLowerCase("") == "");
+}
+
+
+TEST_CASE("52345 in string to integer", "[single-file]") {
+	REQUIRE(parseInt("52345") == 52345);
+}
+
+TEST_CASE("Non integer string to integer", "[single-file]") {
+	REQUIRE(parseInt("noninteger") == 0);
+}
+
+TEST_CASE("Positive integer to string", "[single-file]") {
+	REQUIRE(toString(52345) == "52345");
+}
+
+TEST_CASE("Negative integer to string", "[single-file]") {
+	REQUIRE(toString(-52345) == "-52345");
+}
+
+TEST_CASE("0 integer to string", "[single-file]") {
+	REQUIRE(toString(0) == "0");
+}
